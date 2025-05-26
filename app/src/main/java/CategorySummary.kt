89f9dcd0ee0ca3d2648a74938dd9e019ec3ba674
Veeasha <127542214+Veeasha@users.noreply.github.com>
2025-05-26 16:10:34 +0200

    Delete app directory

--- app/src/main/java/CategorySummary.kt
+++
@@ -1,6 +0,0 @@
-package com.example.spendsense20
-
-data class CategorySummary(
-    val categoryName: String,
-    val totalAmount: Double
-)
--- app/src/main/java/com/example/spendsense20/ProfileEntity.kt
+++
@@ -1,7 +0,0 @@
-package com.example.spendsense20
-
-data class ProfileEntity(
-    var username: String = "",
-    var password: String = "",
-    var email: String = ""
-)
