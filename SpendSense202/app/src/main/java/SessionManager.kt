88501a072ebdb1b3b2dea248f3a3ce0b132736d9
Yadav Priaram <131756403+st10356506@users.noreply.github.com>
2025-06-08 00:26:08 +0200

    Delete SpendSense202 directory

--- SpendSense202/app/src/main/java/SessionManager.kt
+++
@@ -1,6 +0,0 @@
-package com.example.spendsense20
-
-object SessionManager {
-    var userEmail: String? = null
-    var username: String? = null
-}
--- SpendSense202/app/src/main/java/User.kt
+++
@@ -1,7 +0,0 @@
-package com.example.spendsense20
-
-data class User(
-    val username: String = "",
-    val email: String = "",
-    val uid: String = ""
-)
