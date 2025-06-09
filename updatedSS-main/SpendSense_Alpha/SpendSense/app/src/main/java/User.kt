9bca9f377bbe5dbc4c9254b5239e2aa0da2a81c7
Veeasha <veeasha@gmail.com>
2025-06-09 17:51:34 +0200

    Final POE with video link

--- updatedSS-main/SpendSense_Alpha/SpendSense/app/src/main/java/User.kt
+++
@@ -1,7 +0,0 @@
-package com.example.spendsense20
-//user variables
-data class User(
-    val username: String = "",
-    val email: String = "",
-    val uid: String = ""
-)
--- updatedSS-main/SpendSense_Alpha/SpendSense/app/src/main/java/com/example/spendsense20/ProfileEntity.kt
+++
@@ -1,7 +0,0 @@
-package com.example.spendsense20
-//profile variables
-data class ProfileEntity(
-    var username: String = "",
-    var password: String = "",
-    var email: String = ""
-)
